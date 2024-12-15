ebba74d20512a7ca6f9524459acafb34a622e21c
AKashAMode <akashamode80@gmail.com>
2024-12-15 18:26:24 +0530

    created mission and vision page

---
+++ javascript/mission-vision.js
@@ -0,0 +1,40 @@
+let currentIndex = 0;
+
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+function showSlide(index) {
+  if (index >= totalSlides) currentIndex = 0;
+  if (index < 0) currentIndex = totalSlides - 1;
+
+  const offset = -currentIndex * 100;
+  document.querySelector('.slider').style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex--;
+  showSlide(currentIndex);
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex++;
+  showSlide(currentIndex);
+
+
+  clearInterval(autoSlide);
+  autoSlide = setInterval(() => {
+    currentIndex++;
+    showSlide(currentIndex);
+  }, 5000);
+});
+
+
+let autoSlide = setInterval(() => {
+  currentIndex++;
+  showSlide(currentIndex);
+}, 5000);
+
+showSlide(currentIndex);
